23ec8c5bde0eac4e2536fe5cbc8fd1541ba4a53e
“Adam <adam.tabak04@gmail.com>
2026-03-03 23:05:23 +0100

    starting treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,19 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+
+				local ft = vim.bo.filetype
+				if ft and ft ~= "" then
+					pcall(function()
+						require("nvim-treesitter").install({ ft })
+					end)
+				end
+			end,
+		})
+	end,
 }
